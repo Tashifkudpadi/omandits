08bbecf4f2dd492f7c7f3029c2ea8519aea30811
Tashif <imtashifkudpadi@gmail.com>
2023-09-13 23:05:06 +0530

    Initial commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,42 @@
-$('.navTrigger').click(function () {
- $(this).toggleClass('active');
- console.log("Clicked menu");
- $("#mainListDiv").toggleClass("show_list");
- $("#mainListDiv").fadeIn();
-
+$(".navTrigger").click(function () {
+  $(this).toggleClass("active");
+  console.log("Clicked menu");
+  $("#mainListDiv").toggleClass("show_list");
+  $("#mainListDiv").fadeIn();
 });
+
+// Target date
+const targetDate = new Date("2024-03-06T00:00:00");
+
+function updateTimer() {
+  const currentDate = new Date();
+  const timeRemaining = targetDate - currentDate;
+
+  if (timeRemaining <= 0) {
+    document.getElementById("timer").innerHTML = "Happy Valentine's Day!";
+  } else {
+    const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
+    const hours = Math.floor(
+      (timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    const minutes = Math.floor(
+      (timeRemaining % (1000 * 60 * 60)) / (1000 * 60)
+    );
+    const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
+
+    document.getElementById("timer").innerHTML = `
+    <div class="d-flex justify-content-center timer_container">
+                    <p>${days} <br/> <span>Days</span></p>
+                    <p>${hours}<br/> <span> Hours</span></p>
+                    <p>${minutes}<br/> <span>Minutes</span></p>
+                    <p>${seconds}<br/> <span> Seconds</span></p>
+                    </div>
+                `;
+  }
+}
+
+// Update the timer every second
+setInterval(updateTimer, 1000);
+
+// Initial call to update the timer
+updateTimer();
