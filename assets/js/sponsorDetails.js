786f3921affc9a7be3b38b9c2db11d7b344201f4
Amalroy212 <ken940005@gmail.com>
2024-01-04 16:07:37 +0530

    new page added

--- assets/js/sponsorDetails.js
+++ assets/js/sponsorDetails.js
@@ -3,20 +3,18 @@ document.addEventListener("DOMContentLoaded", function() {
     const heading = urlParams.get('heading');
     const logo = urlParams.get('logo');
     const paragraph = urlParams.get("text");
-    const video = urlParams.get('video');
+    const videoParam = urlParams.get('video'); 
 
     const videoContainer = document.getElementById('video-container');
 
-    // console.log(heading, logo, paragraph);
-    console.log(video);
-    if(video === "no video"){
+    if (videoParam === "no video") {
         videoContainer.classList.add("hidden");
-    }else{
-        const video = document.getElementById('video');
-        video.setAttribute('src', video)
+    } else {
+        const videoElement = document.getElementById('video');
+        videoElement.setAttribute('src', videoParam); 
     }
 
-    document.getElementById('title').innerHTML = heading;
+    document.getElementById('heading').innerHTML = heading;
     const logoHolder = document.getElementById('logo');
     logoHolder.setAttribute('src', logo);
     document.getElementById('about').innerHTML = paragraph;
