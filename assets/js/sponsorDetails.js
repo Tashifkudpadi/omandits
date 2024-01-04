554baebe4d37f404426353fc2222fae68c687ef3
Amalroy212 <ken940005@gmail.com>
2024-01-04 15:43:53 +0530

    new video

--- assets/js/sponsorDetails.js
+++ assets/js/sponsorDetails.js
@@ -3,11 +3,21 @@ document.addEventListener("DOMContentLoaded", function() {
     const heading = urlParams.get('heading');
     const logo = urlParams.get('logo');
     const paragraph = urlParams.get("text");
+    const video = urlParams.get('video');
 
-    console.log(heading, logo, paragraph);
+    const videoContainer = document.getElementById('video-container');
+
+    // console.log(heading, logo, paragraph);
+    console.log(video);
+    if(video === "no video"){
+        videoContainer.classList.add("hidden");
+    }else{
+        const video = document.getElementById('video');
+        video.setAttribute('src', video)
+    }
 
     document.getElementById('title').innerHTML = heading;
     const logoHolder = document.getElementById('logo');
     logoHolder.setAttribute('src', logo);
-    document.getElementById('about').innerHTML = paragraph
+    document.getElementById('about').innerHTML = paragraph;
 });
